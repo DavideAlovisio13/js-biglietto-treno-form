da364a127ada99c85d9d6bc16d94f71faf5b2047
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-07 17:13:48 +0100

    fixed name of index

--- js/script.js
+++ js/script.js
@@ -2,20 +2,44 @@ let acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
+
   });
 }
 
 let button = document.getElementById('vai');
-button.addEventListener('click', function(){
-    let userName = (document.getElementById('name').value);
-    document.getElementById('name-ticket').innerHTML = userName;
+button.addEventListener('click', function () {
 
-}
+  let userName = (document.getElementById('name').value);
+  document.getElementById('name-ticket').innerHTML = userName;
+  let userLastName = (document.getElementById('lastname').value);
+  document.getElementById('lastname-ticket').innerHTML = userLastName;
+  let userAge = parseInt(document.getElementById('age').value);
+  document.getElementById('age-ticket').innerHTML = userAge;
+  let userKm = parseInt(document.getElementById('km').value);
+  document.getElementById('km-ticket').innerHTML = userKm;
+  let price = userKm * 0.21;
+  let userExpense = 'value';
+  if (isNaN(userKm) || isNaN(userAge)) {
+    alert('ATTENZIONE! Il valore deve essere un numero')
+  } else {
+    alert('Valori registrati correttamente, premi "OK" per conoscere il prezzo del tuo biglietto.')
+  }
+  if (userAge < 18) {
+    userExpense = price * 0.80;
+  } else if (userAge > 65) {
+    userExpense = price * 0.60;
+  } else {
+    userExpense = price;
+  }
+document.getElementById('expense-ticket').innerHTML = userExpense.toFixed(2);
+
+
+})
