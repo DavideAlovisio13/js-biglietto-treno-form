83d6d7c9d17d76a1263e8825d2ef1909674142a9
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-08 01:32:41 +0100

    release new beta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// compilazione automatica testo
+
+const dynamicText = document.querySelector("h1 span");
+const words = ["Casa", "Scuola", "Il Lavoro", "L'Estero", "Il Mondo", "Ovunque"]
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDelecting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+
+  if (!isDelecting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDelecting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    isDelecting = !isDelecting;
+    wordIndex = !isDelecting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+}
+
+typeEffect();
+
+// accordion pagina principale
+
 let acc = document.getElementsByClassName("accordion");
 let i;
 
@@ -13,6 +44,8 @@ for (i = 0; i < acc.length; i++) {
 
   });
 }
+
+// calcoli 
 
 let button = document.getElementById('vai');
 button.addEventListener('click', function () {
@@ -37,11 +70,22 @@ button.addEventListener('click', function () {
   } else {
     userExpense = price;
   }
+
+  // carrozza e posto a sedere
+
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function getRndInteger2(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let trainNumber = getRndInteger(1000, 6000);
+  let chairNumber = getRndInteger2(10, 100);
+  document.getElementById('chair-ticket').innerHTML = chairNumber;
+  document.getElementById('train-ticket').innerHTML = trainNumber;
   document.getElementById('expense-ticket').innerHTML = userExpense.toFixed(2);
   document.getElementById('ticket').scrollIntoView({
     behavior: 'smooth'
   });
-
-
-
 })
